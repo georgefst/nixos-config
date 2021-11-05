820c119d1a0947c1cff8b5ac30c978ab6c08075a
George Thomas <georgefsthomas@gmail.com>
2021-11-05 01:59:26 +0000

    Initial
    Inspired by:
    - https://eipi.xyz/blog/installing-nixos-on-a-rasberry-pi-3#? (though `boot.kernelPackages = pkgs.linuxPackages_latest` breaks in `generate-config.pl`).
    - `nixos-generate-config --root .`

---
+++ clark.nix
@@ -0,0 +1,39 @@
+# https://nixos.org/manual/nixos/stable/options.html
+{ config, pkgs, lib, ... }:
+{
+  networking.hostName = "clark";
+
+  # stuff I'm probably never going to change
+  system.stateVersion = "21.05"; # it's recommended to keep this at the initial version
+  i18n.defaultLocale = "en_GB.UTF-8";
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+  };
+
+  # users
+  users.users = {
+    root = { };
+    gthomas = {
+      isNormalUser = true;
+      createHome = true;
+      home = "/home/gthomas";
+      extraGroups = [
+        "wheel"
+      ];
+      hashedPassword = "$6$jgaC/YaKr634BoKQ$KIv3VvRRaYShRibX5O3lAaqZ2qE3XRcYQEd0EF6YP61a9YBYUcPtljpDPE8.wEnMDNeUw9/ePBjsrK9JUv5i5/";
+    };
+  };
+
+  # ssh
+  services.openssh.enable = true;
+  users.users.gthomas.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmeBKmTzM4sOaP/JlzyL3VWYDAWn2M2IL55nC1hbaqmz5RT1zG5+LM8vzd1YHxCCdqoTqOtPi3kB0EwGQ2780BP+2zZJxw4hQdqZOuoouBFaZbo5+DHoJErj9mLETEMG3cfJYBw3GxOAn8OdUabETi7tvv3mdblzweKclR08/fECxdcdIte9CqJ9Is3T/XgsXTacl4iPUr74hDZqp1gCwq/rC5Q+cJyZHFdSpeWzUM1p5bxiSFtzB4tLd9JN6phGqFB9cuZWc3IjjEbzxbjzPs46n2oMeS8XC13LvIvkR7AY4x7rei57U1THEx2LxSvMf4bjuXzhsvF7gVRy2qILAe7hGb/6G7gF7thCyDV0z5WYCzvP3Rpj9+57dBXS99yzlaVieHeOI+ODwo6B0t/uW2jZzEnro8zA7KBbaDkBG+WaJtRDRnoWk7kN10AulCFzJgkOoAuantyWE9vM0lThPiMwkRuUgZWvNFu5xPx4rjO/skb1zxVzor/k1HmYw2d0s= gthomas@billy"
+  ];
+
+  environment.systemPackages = [
+    pkgs.tree
+  ];
+}
