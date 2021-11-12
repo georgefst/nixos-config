georgefst/nixos-config

commit ad7b385847bd036454a8bdf1e9a189b25597a6ce

Move socket creation in to the thread that uses it

app/Main.hs
@@ -39,9 +39,9 @@ main = do
         devNames <- traverse (\d -> (d,) <$> sendMessage d GetColor) devs
         pure $ fst <$> find ((== encodeUtf8 optLightName) . label . snd) devNames
 
-    sock <- socket AF_INET Datagram defaultProtocol
-    bind sock $ SockAddrInet 56710 0
     void . forkIO . runLifxUntilSuccess $ forever do
+        sock <- liftIO $ socket AF_INET Datagram defaultProtocol
+        liftIO $ bind sock $ SockAddrInet 56710 0
         bs <- liftIO $ recv sock 1
         withSGR' Blue $ BS.putStrLn $ "Received UDP message: " <> bs
         toggleLight light

commit 5cac16d833260cfb48bf296b74d361b626dc5c90

Avoid recreating socket on each loop iteration
This fixes a serious regression in ad7b385: Move socket creation in to the thread that uses it.

app/Main.hs
@@ -45,12 +45,13 @@ main = do
     Opts{..} <- getRecord "Clark"
     mvar <- newEmptyMVar
 
-    let listenOnNetwork = forever do
+    let listenOnNetwork = do
             sock <- socket AF_INET Datagram defaultProtocol
             bind sock $ SockAddrInet (fromIntegral optReceivePort) 0
-            bs <- recv sock 1
-            withSGR' Blue $ BS.putStrLn $ "Received UDP message: " <> bs
-            putMVar mvar ToggleLight
+            forever do
+                bs <- recv sock 1
+                withSGR' Blue $ BS.putStrLn $ "Received UDP message: " <> bs
+                putMVar mvar ToggleLight
 
     let listenForButton = do
             putStrLn "Starting gpiomon process..."