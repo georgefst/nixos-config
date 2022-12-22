72e404ffc65344832a140918a124044a60835c3e
George Thomas <georgefsthomas@gmail.com>
2022-12-22 01:03:31 +0000

    Make `secrets.nix` empty
    This ensures that it will cause errors. It's pretty disastrous if we actually build with this, as we can't connect to wifi and thus SSH in.

--- secrets.nix
+++ secrets.nix
@@ -1,8 +1,1 @@
 # until we work out a better way of handling secrets, these are stored on a separate local branch
-{
-  wifi.TNCAPA620AF.psk = "";
-  wifi.RML-5ghz.psk = "";
-  passwords.lta = "";
-  mailgun.sandbox = "";
-  mailgun.key = "";
-}
