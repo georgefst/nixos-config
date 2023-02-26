16169410f90840573cf0136ea3c4da45f5fc0ba0
George Thomas <georgefsthomas@gmail.com>
2023-02-26 12:10:58 +0000

    Merge flakes
    As discussed a little in the commit message that introduced the Haskell one, local flakes are a bit of an odd concept, and not well supported. This also allows us to move `cabal.project` up, which gets HLS working.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    clark.url = "path:./clark/haskell";
+    flake-utils.url = "github:numtide/flake-utils";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs-haskell.follows = "haskellNix/nixpkgs-unstable";
     tennis-scraper.url = "/home/gthomas/code/tennis-scraper";
   };
-  outputs = { self, nixpkgs, clark, tennis-scraper }: rec {
+  outputs = { self, nixpkgs, flake-utils, haskellNix, nixpkgs-haskell, tennis-scraper }: rec {
+    haskell = {
+      clark = flake-utils.lib.eachSystem [ "aarch64-linux" ] (system:
+        let
+          overlays = [
+            haskellNix.overlay
+            (final: prev: {
+              hixProject =
+                final.haskell-nix.hix.project {
+                  src = ./.;
+                  compiler-nix-name = "ghc926";
+                  index-state = "2023-02-25T00:00:00Z"; # TODO somehow we still get traces saying this isn't specified
+                  evalSystem = "x86_64-linux";
+                };
+            })
+          ];
+          pkgs = import nixpkgs-haskell { inherit system overlays; inherit (haskellNix) config; };
+          flake = pkgs.hixProject.flake { };
+        in
+        flake // {
+          legacyPackages = pkgs;
+          packages.default = flake.packages."clark:exe:clark";
+        });
+    };
     nixosConfigurations = {
       clark = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -14,7 +39,7 @@
         ];
         specialArgs = {
           extraPkgs = {
-            clark = clark.packages.aarch64-linux.default;
+            clark = haskell.clark.packages.aarch64-linux.default;
             tennis-scraper = tennis-scraper.packages.aarch64-linux.default;
           };
         };
