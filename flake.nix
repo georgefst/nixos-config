e0146842a99d208202b04aae4810744c998a7c1b
George Thomas <georgefsthomas@gmail.com>
2023-04-14 11:34:00 +0100

    DRY Haskell flake configurations

--- flake.nix
+++ flake.nix
@@ -7,15 +7,15 @@
     tennis-scraper = { url = "git+ssh://git@github.com/georgefst/tennis-scraper"; flake = false; };
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }: rec {
-    haskell = {
-      clark = flake-utils.lib.eachSystem [ "aarch64-linux" ] (system:
+    haskell =
+      builtins.mapAttrs (_: { src, default }: flake-utils.lib.eachSystem [ "aarch64-linux" ] (system:
         let
           overlays = [
             inputs.haskellNix.overlay
             (final: prev: {
               hixProject =
                 final.haskell-nix.hix.project {
-                  src = ./.;
+                  inherit src;
                   compiler-nix-name = "ghc927";
                   index-state = "2023-03-19T00:00:00Z";
                   evalSystem = "x86_64-linux";
@@ -27,29 +27,12 @@
         in
         flake // {
           legacyPackages = pkgs;
-          packages.default = flake.packages."clark:exe:clark";
-        });
-      tennis-scraper = flake-utils.lib.eachSystem [ "aarch64-linux" ] (system:
-        let
-          overlays = [
-            inputs.haskellNix.overlay
-            (final: prev: {
-              hixProject =
-                final.haskell-nix.hix.project {
-                  src = inputs.tennis-scraper;
-                  compiler-nix-name = "ghc927";
-                  index-state = "2023-03-19T00:00:00Z";
-                  evalSystem = "x86_64-linux";
-                };
-            })
-          ];
-          pkgs = import inputs.nixpkgs-haskell { inherit system overlays; inherit (inputs.haskellNix) config; };
-          flake = pkgs.hixProject.flake { };
-        in
-        flake // {
-          legacyPackages = pkgs;
-          packages.default = flake.packages."tennis-scraper:exe:tennis-scraper";
-        });
+          packages.default = flake.packages."${default}";
+        })
+      )
+    {
+      clark = { src = ./. ; default = "clark:exe:clark"; };
+      tennis-scraper = { src = inputs.tennis-scraper ; default = "tennis-scraper:exe:tennis-scraper"; };
     };
 
     nixosConfigurations = {
