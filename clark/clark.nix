88695bc05179edd26da3aff4b0a068ddbad1d6a7
George Thomas <georgefsthomas@gmail.com>
2023-08-15 18:19:18 +0100

    basic from docs

--- clark/clark.nix
+++ clark/clark.nix
@@ -62,6 +62,17 @@ in
     (self: super: { })
   ];
 
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   # gpio
   users.groups.gpio = { };
   services.udev.extraRules = ''
