d1cdb6c7d9c99b6c7f8d54b9a9a1eeb66264f554
George Thomas <georgefsthomas@gmail.com>
2023-09-13 16:53:20 +0100

    Use GPIO button for resetting error
    This fixes a mistake from 74df9c2 where I copied too much from `pi`.

--- clark/haskell/app/George/Feed/GPIO.hs
+++ clark/haskell/app/George/Feed/GPIO.hs
@@ -2,8 +2,7 @@ module George.Feed.GPIO (feed, GPIO.Opts (..)) where
 
 import George.Core
 
-import Control.Monad.IO.Class
-import Data.Time
+import Control.Monad.Freer (send)
 import Streamly.Data.Stream.Prelude qualified as S
 import Util.Streamly.GPIO qualified as GPIO
 import Util.Util
@@ -12,6 +11,4 @@ feed :: GPIO.Opts -> S.Stream IO [Event]
 feed opts =
     flip S.mapM (GPIO.stream opts) \case
         GPIO.OutLine{ignoring, line} -> pure [LogEvent $ mwhen ignoring "(Ignoring) " <> line]
-        -- TODO we'd ideally use this is a power button, but for now we just monitor it
-        -- since there have been issues with electrical interference causing spurious triggers
-        GPIO.Event -> pure @[] . LogEvent . ("GPIO button pressed: " <>) . showT <$> liftIO getCurrentTime
+        GPIO.Event -> pure [ActionEvent mempty $ send ResetError]
