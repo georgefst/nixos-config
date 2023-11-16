dc29110a791533aa58b0f04f2ea2248f0eeed74b
George Thomas <georgefsthomas@gmail.com>
2023-11-16 18:15:50 +0000

    Update Okapi for new API, and use GHC 9.6
    This could have been split up, but only a little. New Okapi doesn't build on GHC 9.2 (possibly for trivial reasons). GHC 9.6 doesn't work with the latest Streamly from Hackage. Seeing as both packages are used heavily in `georgefst-utils`, splitting this commit up would have involved a lot of ceremony.
    
    Note that we continue to use GHC 9.2 for the tennis scraper. The `webdriver-w3c` library fails on 9.6, including without Nix.
    
    Note that we have run `nix flake lock --update-input haskellNix`.

--- clark/haskell/app/George/Feed/WebServer.hs
+++ clark/haskell/app/George/Feed/WebServer.hs
@@ -3,18 +3,18 @@ module George.Feed.WebServer (feed, Opts (..)) where
 import George.Core
 import Util
 
-import Control.Applicative
 import Control.Concurrent
-import Control.Monad.Except
+import Control.Monad
 import Control.Monad.Freer
 import Data.Functor
+import Data.Text (Text)
 import Data.Time
 import Data.Word
 import Lifx.Lan (HSBK (..))
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp qualified as Warp
-import Okapi hiding (Event, error, get, head)
-import Okapi qualified hiding (Event, head)
+import Okapi.App hiding (body)
+import Okapi.Response
 import Streamly.Data.Stream.Prelude qualified as S
 import System.Exit
 import Util.Streamly.Okapi qualified as Okapi
@@ -26,46 +26,34 @@ data Opts = Opts
     , lifxMorningKelvin :: Word16
     }
 
--- TODO update to new Okapi as soon as it's released: https://github.com/monadicsystems/okapi/issues/30
 feed :: Opts -> S.Stream IO [Event]
 feed opts =
     S.catMaybes $
         Okapi.stream
             Okapi.Opts
                 { warpSettings = Warp.setPort opts.port Warp.defaultSettings
-                , routes =
-                    [ withGetRoute "reset-error" $ f showT $ send ResetError
-                    , withGetRoute "exit" $ f showT . send . Exit . maybe ExitSuccess ExitFailure =<< optional segParam
-                    , withGetRoute "get-light-power" $ f showT . send . withExists @NullConstraint GetLightPower =<< segParam
-                    , withGetRoute "set-light-power" do
-                        Exists' l <- segParam
-                        p <- segParam
-                        f showT $ send $ SetLightPower l p
-                    , withGetRoute "get-light-colour" $ f showT . send . withExists @NullConstraint GetLightColour =<< segParam
-                    , withGetRoute "set-light-colour" do
-                        Exists' light <- segParam
-                        delay <- segParam @NominalDiffTime -- TODO why do we need this type app?
-                        case light of
-                            Lamp -> do
-                                hue <- segParam
-                                saturation <- segParam
-                                brightness <- segParam
-                                kelvin <- segParam
-                                f showT $ send SetLightColour{colour = HSBK{..}, ..}
-                            Ceiling -> do
-                                brightness <- segParam
-                                kelvin <- segParam
-                                f showT $ send SetLightColourBK{lightBK = light, ..}
-                    , withGetRoute "set-desk-usb-power" $ f showT . send . SetDeskUSBPower =<< segParam
-                    , withGetRoute "send-email" do
-                        subject <- segParam
-                        body <- segParam
-                        f showT $ send SendEmail{..}
-                    , withGetRoute "suspend-laptop" $ f showT $ send SuspendLaptop
-                    , withGetRoute "set-other-led" $ f showT . send . SetOtherLED =<< segParam
-                    , withGetRoute "set-system-leds" $ f showT . send . SetSystemLEDs =<< segParam
-                    , withGetRoute "toggle-ceiling-light" $ f showT toggleCeilingLight
-                    , withGetRoute "sleep-or-wake" $ f showT $ sleepOrWake opts.lifxMorningDelay opts.lifxMorningKelvin
+                , routes = \act ->
+                    [ lit "reset-error" . simpleGet $ f showT act $ send ResetError
+                    , lit "exit" . param . simpleGet $ f showT act . send . Exit . maybe ExitSuccess ExitFailure
+                    , lit "get-light-power" . param . simpleGet . withExists' $ f showT act . send . GetLightPower
+                    , lit "set-light-power" . param . param . simpleGet . withExists' $ f showT act . send .: SetLightPower
+                    , lit "get-light-colour" . param . simpleGet . withExists' $ f showT act . send . GetLightColour
+                    , lit "set-light-colour" $
+                        choice
+                            [ param . param . param . param $
+                                simpleGet \light delay brightness kelvin ->
+                                    f showT act $ send SetLightColourBK{lightBK = light, ..}
+                            , param . param . param . param . param . param $
+                                simpleGet \light delay hue saturation brightness kelvin ->
+                                    f showT act $ send SetLightColour{colour = HSBK{..}, ..}
+                            ]
+                    , lit "set-desk-usb-power" . param . simpleGet $ f showT act . send . SetDeskUSBPower
+                    , lit "send-email" . param . param $ simpleGet \subject body -> f showT act $ send SendEmail{..}
+                    , lit "suspend-laptop" . simpleGet . f showT act $ send SuspendLaptop
+                    , lit "set-other-led" . param . simpleGet $ f showT act . send . SetOtherLED
+                    , lit "set-system-leds" . param . simpleGet $ f showT act . send . SetSystemLEDs
+                    , lit "toggle-ceiling-light" . simpleGet . f showT act $ toggleCeilingLight
+                    , lit "sleep-or-wake" . simpleGet . f showT act $ sleepOrWake opts.lifxMorningDelay opts.lifxMorningKelvin
                     ]
                 }
             <&> \case
@@ -73,10 +61,8 @@ feed opts =
                 Okapi.WarpLog r s i ->
                     guard (not $ statusIsSuccessful s) $> [ErrorEvent (Error "HTTP error" (r, s, i))]
   where
-    f show' a = do
-        m <- liftIO newEmptyMVar
-        pure
-            ( ActionEvent (putMVar m) a
-            , okPlainText [] . (<> "\n") . show' =<< liftIO (takeMVar m)
-            )
-    withGetRoute s x = Okapi.get >> seg s >> x
+    f show' (act :: Event -> IO ()) a ok _req = do
+        m <- newEmptyMVar
+        act $ ActionEvent (putMVar m) a
+        ok noHeaders . (<> "\n") . show' <$> takeMVar m
+    simpleGet = responder @200 @'[] @Text @Text . method GET id
