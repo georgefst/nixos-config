a06131ad7cee9b652a42e8ae513b2770ceb5658a
George Thomas <georgefsthomas@gmail.com>
2023-11-16 20:49:42 +0000

    factor out with emits

--- clark/haskell/app/George/Feed/WebServer.hs
+++ clark/haskell/app/George/Feed/WebServer.hs
@@ -6,8 +6,11 @@ import Util
 import Control.Concurrent
 import Control.Monad
 import Control.Monad.Freer
+import Control.Monad.IO.Class
+import Data.Functor
 import Data.Text (Text)
 import Data.Time
+import Data.Tuple.Extra
 import Data.Word
 import Lifx.Lan (HSBK (..))
 import Network.HTTP.Types
@@ -27,41 +30,63 @@ data Opts = Opts
     }
 
 feed :: Opts -> S.Stream IO [Event]
-feed opts = S.fromEmitter \emit ->
-    let warpLog r s i = unless (statusIsSuccessful s) $ emit [ErrorEvent (Error "HTTP error" (r, s, i))]
-        f :: (Show r) => (r -> Text) -> CompoundAction r -> (Headers '[] -> Text -> Wai.Response) -> Wai.Request -> IO Wai.Response
-        f show' a ok _req = do
-            m <- newEmptyMVar
-            emit $ pure $ ActionEvent (putMVar m) a
-            ok noHeaders . (<> "\n") . show' <$> takeMVar m
-        wrap = responder @200 @'[] @Text @Text . method GET id
-     in Warp.runSettings (Warp.setLogger warpLog (Warp.setPort opts.port Warp.defaultSettings)) $
-            withDefault
-                ( choice
-                    [ lit "reset-error" . wrap $ f showT $ send ResetError
-                    , lit "exit" . param . wrap $ f showT . send . Exit . maybe ExitSuccess ExitFailure
-                    , lit "get-light-power" . param . wrap . withExists' $ f showT . send . GetLightPower
-                    , lit "set-light-power" . param . param . wrap . withExists' $ f showT . send .: SetLightPower
-                    , lit "get-light-colour" . param . wrap . withExists' $ f showT . send . GetLightColour
-                    , lit "set-light-colour" $
-                        choice
-                            [ param . param . param . param $
-                                wrap \light delay brightness kelvin ->
-                                    f showT $ send SetLightColourBK{lightBK = light, ..}
-                            , param . param . param . param . param . param $
-                                wrap \light delay hue saturation brightness kelvin ->
-                                    f showT $ send SetLightColour{colour = HSBK{..}, ..}
-                            ]
-                    , lit "set-desk-usb-power" . param . wrap $ f showT . send . SetDeskUSBPower
-                    , lit "send-email" . param . param $ wrap \subject body -> f showT $ send SendEmail{..}
-                    , lit "suspend-laptop" . wrap . f showT $ send SuspendLaptop
-                    , lit "set-other-led" . param . wrap $ f showT . send . SetOtherLED
-                    , lit "set-system-leds" . param . wrap $ f showT . send . SetSystemLEDs
-                    , lit "toggle-ceiling-light" . wrap . f showT $ toggleCeilingLight
-                    , lit "sleep-or-wake" . wrap . f showT $ sleepOrWake opts.lifxMorningDelay opts.lifxMorningKelvin
+feed opts =
+    S.catMaybes $
+        stream' @IO @Event
+            Opts'
+                { warpSettings = Warp.setPort opts.port Warp.defaultSettings
+                , routes =
+                    [ \emit -> lit "reset-error" . wrap $ f showT emit $ send ResetError
+                    , \emit -> lit "exit" . param . wrap $ f showT emit . send . Exit . maybe ExitSuccess ExitFailure
+                    , \emit -> lit "get-light-power" . param . wrap . withExists' $ f showT emit . send . GetLightPower
+                    , \emit -> lit "set-light-power" . param . param . wrap . withExists' $ f showT emit . send .: SetLightPower
+                    , \emit -> lit "get-light-colour" . param . wrap . withExists' $ f showT emit . send . GetLightColour
+                    , \emit ->
+                        lit "set-light-colour" $
+                            choice
+                                [ param . param . param . param $
+                                    wrap \light delay brightness kelvin ->
+                                        f showT emit $ send SetLightColourBK{lightBK = light, ..}
+                                , param . param . param . param . param . param $
+                                    wrap \light delay hue saturation brightness kelvin ->
+                                        f showT emit $ send SetLightColour{colour = HSBK{..}, ..}
+                                ]
+                    , \emit -> lit "set-desk-usb-power" . param . wrap $ f showT emit . send . SetDeskUSBPower
+                    , \emit -> lit "send-email" . param . param $ wrap \subject body -> f showT emit $ send SendEmail{..}
+                    , \emit -> lit "suspend-laptop" . wrap . f showT emit $ send SuspendLaptop
+                    , \emit -> lit "set-other-led" . param . wrap $ f showT emit . send . SetOtherLED
+                    , \emit -> lit "set-system-leds" . param . wrap $ f showT emit . send . SetSystemLEDs
+                    , \emit -> lit "toggle-ceiling-light" . wrap . f showT emit $ toggleCeilingLight
+                    , \emit -> lit "sleep-or-wake" . wrap . f showT emit $ sleepOrWake opts.lifxMorningDelay opts.lifxMorningKelvin
                     ]
-                )
-                \_req resp -> resp $ Wai.responseLBS status404 [] "Not Found..."
+                }
+            <&> \case
+                Event' x -> Just [x]
+                WarpLog' r s i ->
+                    guard (not $ statusIsSuccessful s) $> [ErrorEvent (Error "HTTP error" (r, s, i))]
+  where
+    f :: (Show r) => (r -> Text) -> (Event -> IO ()) -> CompoundAction r -> (Headers '[] -> Text -> Wai.Response) -> Wai.Request -> IO Wai.Response
+    f show' emit a ok _req = do
+        m <- newEmptyMVar
+        emit $ ActionEvent (putMVar m) a
+        ok noHeaders . (<> "\n") . show' <$> takeMVar m
+    wrap = responder @200 @'[] @Text @Text . method GET id
+
+data Opts' a = Opts'
+    { warpSettings :: Warp.Settings -- TODO what if the settings passed in override the logger? just say not to in Haddocks?
+    , routes :: [(a -> IO ()) -> Node '[]]
+    }
+data Item' a
+    = Event' a
+    | WarpLog' Wai.Request Status (Maybe Integer)
+stream' ::
+    (MonadIO m) =>
+    Opts' a ->
+    S.Stream m (Item' a)
+stream' Opts'{..} = S.morphInner liftIO $ S.fromEmitter \f ->
+    Warp.runSettings (Warp.setLogger (curry3 $ f . uncurry3 WarpLog') warpSettings)
+        . withDefault (choice $ map ($ (f . Event')) routes)
+        $ \_ resp -> resp $ Wai.responseLBS status404 [] "Not Found..."
 
 infixr 8 .:
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
