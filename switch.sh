e3d3de5705820caff2212fa1b3e9f09c717c6631
George Thomas <georgefsthomas@gmail.com>
2025-10-09 00:17:17 +0100

    Avoid specifying host name for rebuild
    The default behaviour of `nixos-rebuild` takes care of this.

--- switch.sh
+++ switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if realpath "$(which nixos-rebuild)" | grep -q "nixos-rebuild-ng"; then
-    nixos-rebuild switch --flake .#$(hostname) --sudo
+    nixos-rebuild switch --flake . --sudo
 else
-    nix shell nixpkgs#nixos-rebuild-ng -c nixos-rebuild-ng switch --flake .#$(hostname) --sudo
+    nix shell nixpkgs#nixos-rebuild-ng -c nixos-rebuild-ng switch --flake . --sudo
 fi
