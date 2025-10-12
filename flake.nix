622664cdc3eb7bfa9de6b5de20f44cfde8c86c12
George Thomas <georgefsthomas@gmail.com>
2025-10-12 16:08:54 +0100

    Simplify local Haskell project and enable dev shell
    Bizarrely, moving the Haskell code here results in building GHC, with `nix build .#configs.clark`. It is possible this is because of a manual `nix copy` where I moved the output store path from an old machine but not its build dependencies.

--- flake.nix
+++ flake.nix
@@ -13,19 +13,17 @@
     hs-scripts.url = "github:georgefst/hs-scripts/nix";
     self.submodules = true;
   };
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, flake-utils, agenix, ... }: rec {
-    haskell =
-      builtins.mapAttrs
-        (name: src: (flake-utils.lib.eachDefaultSystem (system:
-          let
-            flake = (import inputs.nixpkgs-haskell {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-hardware, flake-utils, agenix, ... }:
+    let
+      haskell = flake-utils.lib.eachDefaultSystem (system:
+            (import inputs.nixpkgs-haskell {
               inherit system;
               overlays = [
                 inputs.haskellNix.overlay
                 (final: prev: {
                   hixProject =
                     final.haskell-nix.hix.project {
-                      inherit src;
+                      src = ./.;
                       compiler-nix-name = "ghc9122";
                       index-state = "2025-09-02T00:00:00Z";
                       evalSystem = "x86_64-linux";
@@ -33,19 +31,10 @@
                 })
               ];
               inherit (inputs.haskellNix) config;
-            }).hixProject.flake { };
-            default = "${name}:exe:${name}"; # only factored out because of issues with vscode syntax highlighter
-          in
-          flake // {
-            packages = flake.packages // { default = flake.packages."${default}"; };
-          }
-        )))
-        {
-          clark = ./.;
-        };
+            }).hixProject.flake { });
 
-    nixosConfigurations =
-      let
+      evdev-share = system: inputs.evdev-share.packages.${system}.default;
+
         mandelbrot = system: pkgs: { xMin, xMax, yMin, yMax }: pkgs.runCommand "mandelbrot" { } ''
           ${pkgs.lib.getExe inputs.hs-scripts.packages.${system}.mandelbrot} \
             --width 3840 --height 3840 \
@@ -53,8 +42,8 @@
             --yMin ${builtins.toString yMin} --yMax ${builtins.toString yMax} \
             --out $out
         '';
-      in
-      {
+
+      nixosConfigurations = {
         clark =
           let
             system = "aarch64-linux";
@@ -69,10 +58,10 @@
               agenix.nixosModules.default
             ];
             specialArgs = {
-              extraPkgs = builtins.listToAttrs
-                (map (name: { inherit name; value = (inputs // haskell)."${name}".packages."${system}".default; })
-                  [ "clark" "evdev-share" ]
-                );
+              extraPkgs = {
+                clark = haskell.packages.${system}."clark:exe:clark";
+                evdev-share = evdev-share system;
+              };
             };
           };
         fry =
@@ -142,8 +131,11 @@
           };
       };
 
-    images = builtins.mapAttrs (_: system: system.config.system.build.sdImage) nixosConfigurations;
-    configs = builtins.mapAttrs (_: system: system.config.system.build.toplevel) nixosConfigurations;
-    vms = builtins.mapAttrs (_: system: system.config.system.build.vm) nixosConfigurations;
-  };
+    in
+    haskell // {
+      inherit nixosConfigurations;
+      images = builtins.mapAttrs (_: system: system.config.system.build.sdImage) nixosConfigurations;
+      configs = builtins.mapAttrs (_: system: system.config.system.build.toplevel) nixosConfigurations;
+      vms = builtins.mapAttrs (_: system: system.config.system.build.vm) nixosConfigurations;
+    };
 }
