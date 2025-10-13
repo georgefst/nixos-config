36235f2f1b0f6920afdd7b18a15dbac7842e3a01
George Thomas <georgefsthomas@gmail.com>
2025-10-13 13:13:31 +0100

    Add keybinding for copying current line from terminal
    Ideally, it'd be great to be able to navigate output of long-running commands without using the mouse as well, but realistically that would probably mean ditching Gnome's terminal which I otherwise like, as this bug has been open for 23 years: https://gitlab.gnome.org/GNOME/vte/-/issues/588.
    
    This is the only sensible keybinding which seemed to work, as any combination with ctrl-c killed commands, and the common ctrl-p conflicts with VSCode.

--- util/common.nix
+++ util/common.nix
@@ -35,6 +35,9 @@
     dots=$(printf '%*s' $((SHLVL - 1 - $vscode)) | tr ' ' '.')
     PS1="\[$bold\]\[$blue\]\H\[$reset\]\[$bold\]:\[$green\]\w\[$reset\]\[$bold\]\$dots\$ \[$reset\]"
   '';
+  programs.bash.interactiveShellInit = ''
+    bind -x '"\ec": printf "%s" "$READLINE_LINE" | wl-copy'
+  '';
   services.openssh.settings.PasswordAuthentication = false;
   users.users.gthomas.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUnvz6Q8zIzqbIG2iy72u6zl5Xg/tem1r93G3FNwGF9 gthomas@billy"
