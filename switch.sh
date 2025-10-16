c46f9e117a44dc7379a787ad07de717219c9e936
George Thomas <georgefsthomas@gmail.com>
2025-10-16 11:23:21 +0100

    Take optional version argument in switch script

--- switch.sh
+++ switch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -n "$1" ]
+then
+    REV="?rev=$(git rev-parse $1)"
+fi
+
 if realpath "$(which nixos-rebuild)" | grep -q "nixos-rebuild-ng"; then
-    nixos-rebuild switch --flake . --sudo
+    nixos-rebuild switch --flake .$REV --sudo
 else
     nix shell nixpkgs#nixos-rebuild-ng -c nixos-rebuild-ng switch --flake . --sudo
 fi
