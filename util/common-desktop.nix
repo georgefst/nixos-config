3a37c0182c56048dbb40542ca17a3ecb4f8ecf15
George Thomas <georgefsthomas@gmail.com>
2025-11-06 15:53:55 +0000

    Format

--- util/common-desktop.nix
+++ util/common-desktop.nix
@@ -178,7 +178,7 @@ in
             ];
         in
         { "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = map (b: "/${b.name}/") bindings; }
-        // lib.listToAttrs bindings
+          // lib.listToAttrs bindings
       );
     }
   ];
