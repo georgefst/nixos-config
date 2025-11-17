855e8229c3d878440cbd3e0c1c5a2b20b37dabeb
George Thomas <georgefsthomas@gmail.com>
2025-11-17 02:37:11 +0000

    remove rebuild ng shell now that on unstable

--- switch.sh
+++ switch.sh
@@ -5,8 +5,4 @@ then
     REV="?rev=$(git rev-parse $1)"
 fi
 
-if realpath "$(which nixos-rebuild)" | grep -q "nixos-rebuild-ng"; then
-    nixos-rebuild switch --flake .$REV --sudo
-else
-    nix shell nixpkgs#nixos-rebuild-ng -c nixos-rebuild-ng switch --flake . --sudo
-fi
+nixos-rebuild switch --flake .$REV --sudo
