895327d018bec2bbd270f8b8c29ac8235d4d17f2
George Thomas <georgefsthomas@gmail.com>
2025-11-22 21:56:52 +0000

    Rename asset folder

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -189,7 +189,7 @@ in
         name = "gather";
         desktopName = "Gather";
         exec = "${lib.getExe chromium} --app=https://app.v2.gather.town/app/obsidian-3812d4d3-1a3e-4e30-b603-b31c7b22e94f";
-        icon = "${../media/gather.png}";
+        icon = "${../assets/gather.png}";
       };
       ghc = haskellPackages.ghcWithPackages (import ./haskell-libs.nix);
       spotify = pkgs.spotify.overrideAttrs (old: {
